246d8158c9ac442cd6fe99ab91764bfb45375430
bosu-301 <js38911@hanyang.ac.kr>
2020-06-12 16:48:50 +0900

    home components define

---
+++ src/components/home/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- <img alt="Feelm logo" src="../assets/logo.png"> -->
+    <div class="d-inline">
+        사용자 관련 정보
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+    
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="@/assets/logo.png"> -->
+    <NavBar/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '../components/home/NavBar.vue'
+// import RecentTrailerPlayer from '@/components/RecentTrailerPlayer.vue'
+// import MovieCarousel from '@/components/MovieCarousel.vue'
+// import HomeReviewList from '@/components/HomeReviewList.vue'
+// import HomeCommentList from '@/components/HomeCommentList.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    NavBar,
+    // RecentTrailerPlayer,
+    // MovieCarousel,
+    // ReviewList,
+    // CommentList,
   }
 }
 </script>
