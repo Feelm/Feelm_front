c640246c8c42e88ffba81123e226747e267868dc
bosu-301 <js38911@hanyang.ac.kr>
2020-06-12 22:52:33 +0900

    회원가입 vuex

---
+++ src/components/home/RecentTrailerPlayer.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: "RecentTrailerPlayer",
+    data() {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,49 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import cookies from 'vue-cookies'
+import axios from 'axios'
+import router from '@/router'
+import SERVER from '@/api/djangoAPI'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    authToken: cookies.get('auth-token'),
   },
   mutations: {
+    SET_TOKEN(state, token) {
+      state.authToken = token
+      cookies.set('auth-token', token)
+    },
   },
   actions: {
+    postAuthData({ commit }, info) {
+      console.log(SERVER.URL + info.location, 'info.data',info.data)
+      console.log('test')
+      axios.post(SERVER.URL + info.location, info.data)
+        .then(res => {
+          commit('SET_TOKEN', res.data.key)
+          router.push({ name: '' })
+        })
+        .catch(err => console.log(err.response.data))
+    },
+    login({ dispatch }, loginData) {
+      const info = {
+        data: loginData,
+        location: SERVER.ROUTES.login,
+      }
+      dispatch('postAuthData', info)
+    },
+    signup({ dispatch }, signupData) {
+      const info = {
+        data: signupData,
+        location: SERVER.ROUTES.signup
+      }
+      dispatch('postAuthData', info)
+    },
   },
   modules: {
-  }
+  },
 })
