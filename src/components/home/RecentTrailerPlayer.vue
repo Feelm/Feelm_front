9cc8804747d20d1923eaa1ff9ce6228de5d837a7
bosu-301 <js38911@hanyang.ac.kr>
2020-06-14 20:23:30 +0900

    trailerPlayer backscreen

--- src/components/home/RecentTrailerPlayer.vue
+++ src/components/home/RecentTrailerPlayer.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="iframebox d-block" v-if="$store.state.videoId">
     <iframe 
-        :src="videoURL+'?autoplay=1&loop=1'" frameborder="0" 
+        :src="videoURL+'?autoplay=1&loop=1&fs=0&disablekb=1&modestbranding=1&showinfo=0&controls=0'" frameborder="0" 
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
         onload="doResize()" width="100%" height="100%"
         allowfullscreen
     ></iframe>
+    <div class="backscreen">
+    <div class="row backscreenrow">
+        <div class="col-6"></div>
+        <div class="col-6"></div>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -37,5 +43,18 @@ export default {
     }
     .iframebox iframe {
         position: absolute;
+    }
+    .backscreen {
+        position: absolute;
+        width: 100%;
+        /* padding-bottom: 56.25%; */
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        
+    }
+    .backscreenrow{
+        position: relative;
+        width: 100%;
+        height: 100%;
     }
 </style>
