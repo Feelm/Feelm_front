899f677e8ae908624a4fd70a7613da9e19ff7eb2
hojin <redsmile123@naver.com>
2020-06-17 16:00:06 +0900

    시간 표시 변경

--- src/components/board/FreeBoard.vue
+++ src/components/board/FreeBoard.vue
@@ -12,59 +12,30 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="board in boardList" :key="board.id">
-          <th scope="row">{{board.id}}</th>
-          <td>{{board.title}}</td>
-          <td>{{board.user}}</td>
-          <td>{{board.like}}</td>
-          <td>{{board.view_count}}</td>
-          <td>{{board.created_at}}</td>
-        </tr>
+        <FreeBoardEach v-for="board in boardList" :key="board.id" :board="board" />
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
-import SERVER from '@/api/djangoAPI'
-import axios from 'axios'
+import FreeBoardEach from '@/components/board/FreeBoardEach.vue'
 
 export default {
- name: 'FreeBoard',
- props: {
-   boardList: {
-     type : Array,
-   }
- },
- methods: {
-   movieRequest(movie_id,request_id){
-     const url= SERVER.URL+SERVER.ROUTES.getMovies+movie_id+'/'
-     const checkUrl = SERVER.URL + SERVER.ROUTES.boards+SERVER.ROUTES.requests
-     axios.post(url)
-      .then( res => {
-        console.log(res.data)
-        axios.put(checkUrl,{'request_id':request_id},null)
-          .then(res2 => {
-            console.log(res2.data)
-            const tr = document.querySelector(`.input${request_id}`)
-            tr.parentElement.parentElement.className= `tr_check_true`
-          })
-          .catch( err2 => console.log(err2.response))
-      })
-      .catch( err => console.log(err.response))
-   },
- },
-
- computed: {
-   is_admin(){
-     return this?.$store?.state?.userInfo?.is_superuser
-   },
- },
+  name: 'FreeBoard',
+  props: {
+    boardList: {
+      type : Array,
+    }
+  },
+  methods: {
+  },
+  components :{
+    FreeBoardEach,
+  },
 }
 </script>
 
-<style scoped>
-  .tr_check_true{
-    text-decoration: line-through;
-  }
+<style>
+
 </style>
---
+++ src/components/board/FreeBoardEach.vue
@@ -0,0 +1,46 @@
+<template>
+  <tr>
+    <th scope="row">{{board.id}}</th>
+      <td>{{board.title}}</td>
+      <td>{{board.user}}</td>
+      <td>{{board.like}}</td>
+      <td>{{board.view_count}}</td>
+      <td>{{displayTime}}
+    </td>
+  </tr>
+</template>
+
+<script>
+import { formatDistance } from 'date-fns'
+import { ko } from 'date-fns/locale/'
+
+export default {
+  name: 'FreeBoardEach',
+  props: {
+    board: {
+      type : Object,
+    }
+  },
+  computed: {
+    displayTime(){
+      if(this.board.created_at === this.board.updated_at){
+        return formatDistance( new Date(this?.board?.created_at),new Date(),{
+            includeSeconds: true,
+            addSuffix: true,
+            locale : ko,
+          })
+      } else {
+        return `${formatDistance( new Date(this?.board?.updated_at),new Date(),{
+            includeSeconds: true,
+            addSuffix: true,
+            locale : ko,
+          })}수정`
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
